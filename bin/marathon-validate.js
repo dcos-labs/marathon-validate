1563339e08675c36cade7cb0634599ee637c17c2
tobilg <tobilg@gmail.com>
2016-12-15 12:09:18 +0100

    Refactoring, adding tag list feature

--- bin/marathon-validate.js
+++ bin/marathon-validate.js
@@ -3,10 +3,11 @@
 "use strict";
 
 var pkg = require("../package.json");
-var program = require("commander");
 var path = require("path");
+var os = require("os");
+var program = require("commander");
 var Schema = require("../lib/schema");
-var request = require("request");
+var Tags = require("../lib/tags");
 var filePath = "";
 
 program
@@ -18,42 +19,60 @@ program
     .option("-t, --tags", "Get a list of tags for the Marathon project")
     .parse(process.argv);
 
-if (program.args.length !== 1) {
-    console.log("Please specify a file to validate!");
-    process.exit(1);
-} else {
-    if (path.isAbsolute(program.args[0])) {
-        filePath = program.args[0];
-    } else {
-        filePath = path.normalize(path.join(process.cwd(), program.args[0]));
-    }
-}
-
-if (program.app || program.group) {
-
-    var type = "";
-
-    // Set type
-    if (program.app) {
-        type = "app";
-    } else if (program.group) {
-        type = "group";
-    }
-
-    var version = program.marathon || "master";
-
-    var schema = new Schema(type, version, filePath);
-
-    schema.validate(function (error, result) {
+if (program.tags) {
+    // https://api.github.com/repos/mesosphere/marathon/tags?per_page=100
+    var tags = new Tags();
+    tags.getList(function(error, tags) {
         if (error) {
-            console.log(error.message);
+            console.log(error);
             process.exit(1);
-        } else if (result) {
-            console.log(" --> OK! The file '" + filePath + "' is valid!");
-            process.exit(0);
+        } else {
+            if (tags.length > 0) {
+                console.log(" --> List of tags:");
+                console.log(tags.join(os.EOL));
+            }
         }
-    });
+    })
 } else {
-    console.log("Please either use the --app or --group flags!");
-    process.exit(1);
+
+    if (program.args.length !== 1) {
+        console.log("Please specify a file to validate!");
+        process.exit(1);
+    } else {
+        if (path.isAbsolute(program.args[0])) {
+            filePath = program.args[0];
+        } else {
+            filePath = path.normalize(path.join(process.cwd(), program.args[0]));
+        }
+    }
+
+    if (program.app || program.group) {
+
+        var type = "";
+
+        // Set type
+        if (program.app) {
+            type = "app";
+        } else if (program.group) {
+            type = "group";
+        }
+
+        var version = program.marathon || "master";
+
+        var schema = new Schema(type, version, filePath);
+
+        schema.validate(function (error, result) {
+            if (error) {
+                console.log(error.message);
+                process.exit(1);
+            } else if (result) {
+                console.log(" --> OK! The file '" + filePath + "' is valid!");
+                process.exit(0);
+            }
+        });
+    } else {
+        console.log("Please either use the --app or --group flags!");
+        process.exit(1);
+    }
+
 }
